074fe8b1fa79717a17aa582a10f5c8f066deb30f
ffalt <ffalty@gmail.com>
2017-07-24 17:35:45 +0200

    build geojson with nuts names

---
+++ shared/nuts/source/build_nuts.js
@@ -0,0 +1,33 @@
+let fs = require('fs');
+let namelist = JSON.parse(fs.readFileSync('./NUTS_AT_2013.json').toString());
+
+let names = {};
+namelist.forEach(name => {
+	names[name.NUTS_ID] = name.NAME_LATN;
+});
+
+fs.writeFileSync('../nuts_names.json', JSON.stringify(names, null, '\t'));
+
+let buildGeoJson = (filename, resolution, level) => {
+	console.log('file', filename);
+	let geo = JSON.parse(fs.readFileSync(filename).toString());
+	geo.features.forEach(feature => {
+		let name = names[feature.properties.NUTS_ID];
+		if (!name) {
+			console.log('warning, unnamed NUTS code', feature.properties.NUTS_ID);
+			name = 'NUTS-' + feature.properties.NUTS_ID;
+		}
+		feature.properties = {id: feature.properties.NUTS_ID, name};
+		// console.log(feature);
+	});
+
+	fs.writeFileSync('../nuts_' + resolution + 'M_lvl' + level + '.geojson', JSON.stringify(geo));//, null, '\t'));
+};
+
+['20', '60'].forEach((resolution) => {
+	[1, 2, 3].forEach((level) => {
+		buildGeoJson('./nuts_rg_' + resolution + 'M_2013_lvl_' + level + '.geojson', resolution, level);
+	});
+});
+
+
