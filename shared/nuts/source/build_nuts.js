dc84ac97dcdbc07fd4034f277499c13e64c8d5e7
ffalt <ffalty@gmail.com>
2017-10-24 16:32:22 +0200

    more nuts names

--- shared/nuts/source/build_nuts.js
+++ shared/nuts/source/build_nuts.js
@@ -1,9 +1,32 @@
 let fs = require('fs');
 let namelist = JSON.parse(fs.readFileSync('./NUTS_AT_2013.json').toString());
+let tabbedlist = fs.readFileSync('./NUTS_2013L.csv').toString().split('\n');
+
 
 let names = {};
 namelist.forEach(name => {
 	names[name.NUTS_ID] = name.NAME_LATN;
+});
+
+tabbedlist.forEach(line => {
+	let col = line.split('|');
+	if (col[4]) {
+		let nut = col[4].slice(1, col[4].length - 1);
+		let name = col[5].slice(1, col[5].length - 1);
+		names[nut] = name;
+	}
+});
+
+let list = Object.keys(names).map(key => {
+	return {name: key, value: names[key]};
+}).sort((a, b) => {
+	if (a.name < b.name) return -1;
+	if (a.name > b.name) return 1;
+	return 0;
+});
+names = {};
+list.forEach(item => {
+	names[item.name] = item.value;
 });
 
 fs.writeFileSync('../nuts_names.json', JSON.stringify(names, null, '\t'));
